17bc79d58ea4e8ac450e8618b17d595eb07ea19e
Ekeopara-Praise <ekeoparapraise@gmail.com>
2025-09-01 23:37:11 +0100

    Added Python Refresher

---
+++ Python-Refresher/Introduction_1.py
@@ -0,0 +1,2 @@
+import sys
+print(sys.version)  # Print the Python version
--- src/testlearn/operations.py
+++ src/testlearn/operations.py
@@ -1,7 +1,7 @@
 def add(n1, n2):
-    
+    # Returns the sum of n1 and n2
     return n1 + n2
 
 def subtract(n1, n2):
-
+    # Returns the difference between n1 and n2
     return n1 - n2
